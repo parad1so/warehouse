2a75ce49b5374dd52210f02e5c48bffb9f066c41
parad1so <job@seoweblab.ru>
2022-10-31 21:42:26 +0300

    war

--- src/main/java/com/warehouse/stockinventory/StockInventoryApplication.java
+++ src/main/java/com/warehouse/stockinventory/StockInventoryApplication.java
@@ -3,6 +3,7 @@ package com.warehouse.stockinventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @ComponentScan(basePackages = "com.warehouse")
 @EnableJpaRepositories(basePackages = "com.warehouse.dao.spring")
 @EntityScan(basePackages = "com.warehouse.model")
-public class StockInventoryApplication {
+public class StockInventoryApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StockInventoryApplication.class, args);
